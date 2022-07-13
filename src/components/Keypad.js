f6b510336e28ed7e409215866b1eaf6be2a60b2c
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-13 12:32:05 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad(){
+    function handleChange(){
+        console.log('Entering password...')
+    }
+
+    return(
+        <>
+        <input onChange={handleChange}   type="password"  placeholder="password" />
+        </>
+    )
+}
+export default Keypad
